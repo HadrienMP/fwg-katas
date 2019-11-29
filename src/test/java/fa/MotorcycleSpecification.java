e6e64ff638566931259482e38bebc4ab11ea7d1a
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-29 15:46:05 +0100

    Simplify the tests

--- src/test/java/fa/MotorcycleSpecification.java
+++ src/test/java/fa/MotorcycleSpecification.java
@@ -19,7 +19,7 @@ public class MotorcycleSpecification {
     public void the_price_is_1_euros_for_a_stay_of_2_started_hours(int minutes) {
         LocalDateTime arrival = LocalDateTime.now();
         LocalDateTime departure = arrival.plusMinutes(minutes);
-        ParkingTicket parkingTicket = new ParkingTicket(arrival, departure, "Motorcycle" != null);
+        ParkingTicket parkingTicket = new ParkingTicket(arrival, departure, true);
 
         assertThat(parkingTicket.price()).isEqualTo(100);
     }
@@ -31,7 +31,7 @@ public class MotorcycleSpecification {
     public void the_first_hour_is_free(int minutes) {
         LocalDateTime arrival = LocalDateTime.now();
         LocalDateTime departure = arrival.plusMinutes(minutes);
-        ParkingTicket parkingTicket = new ParkingTicket(arrival, departure, "Motorcycle" != null);
+        ParkingTicket parkingTicket = new ParkingTicket(arrival, departure, true);
 
         assertThat(parkingTicket.price()).isEqualTo(0);
     }
