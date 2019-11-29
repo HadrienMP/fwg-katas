937b3afe8b200ec527c1541a19bc6845c510f981
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-29 14:30:18 +0100

    use 4 hours in the code instead of 3 (more accurate)

--- src/test/java/fa/Specification.java
+++ src/test/java/fa/Specification.java
@@ -88,7 +88,7 @@ public class Specification {
     private static class ParkingTicket {
         private static final long FULL_HOUR_PRICE = 200L;
         private static final long HALF_HOUR_PRICE = 150L;
-        private static final int MAXIMUM_NUMBER_OF_HOURS_TO_PRICE_IN_FULL = 4;
+        private static final int FULL_HOUR_PRICE_TIME_LIMIT = 4;
         private final LocalDateTime arrival;
         private final LocalDateTime departure;
 
@@ -100,9 +100,8 @@ public class Specification {
         private Long price() {
             TimeInParking timeInParking = new TimeInParking(Duration.between(arrival, departure));
 
-            long billableHours = Math
-                    .min(MAXIMUM_NUMBER_OF_HOURS_TO_PRICE_IN_FULL, timeInParking.startedHours()) - 1;
-            long billableHalfHours = timeInParking.halfHoursAfter(Duration.ofHours(MAXIMUM_NUMBER_OF_HOURS_TO_PRICE_IN_FULL));
+            long billableHours = Math.min(FULL_HOUR_PRICE_TIME_LIMIT, timeInParking.startedHours()) - 1;
+            long billableHalfHours = timeInParking.halfHoursAfter(Duration.ofHours(FULL_HOUR_PRICE_TIME_LIMIT));
             return billableHours * FULL_HOUR_PRICE + billableHalfHours * HALF_HOUR_PRICE;
         }
 
