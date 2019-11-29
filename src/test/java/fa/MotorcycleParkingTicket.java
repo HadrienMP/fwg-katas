5b3bd8e70c480c3c445427a176d6d5c9467afec4
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-29 15:48:56 +0100

    remove duplication

--- src/test/java/fa/MotorcycleParkingTicket.java
+++ src/test/java/fa/MotorcycleParkingTicket.java
@@ -4,31 +4,16 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 class MotorcycleParkingTicket {
-    private static final long FULL_HOUR_PRICE = 200L;
-    private static final long HALF_HOUR_PRICE = 150L;
-    private static final Duration FULL_HOUR_PRICE_TIME_LIMIT = Duration.ofHours(4);
     private final LocalDateTime arrival;
     private final LocalDateTime departure;
-    private boolean motoSmell;
 
-    public MotorcycleParkingTicket(LocalDateTime arrival, LocalDateTime departure, boolean motoSmell) {
+    public MotorcycleParkingTicket(LocalDateTime arrival, LocalDateTime departure) {
         this.arrival = arrival;
         this.departure = departure;
-        this.motoSmell = motoSmell;
     }
 
     Long price() {
-        if (motoSmell) return new MotorcycleParkingTicket(arrival, departure, false).price() / 2;
-
-        TimeInParking timeInParking = timeInParking();
-
-        long billableHours = timeInParking.startedHoursUpTo(FULL_HOUR_PRICE_TIME_LIMIT);
-        long billableHalfHours = timeInParking.halfHoursAfter(FULL_HOUR_PRICE_TIME_LIMIT);
-        return billableHours * FULL_HOUR_PRICE + billableHalfHours * HALF_HOUR_PRICE;
-    }
-
-    private TimeInParking timeInParking() {
-        return TimeInParking.of(arrival, departure);
+        return new ParkingTicket(arrival, departure, false).price() / 2;
     }
 
 }
