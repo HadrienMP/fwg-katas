b2e7a315d5554103f9f2c9fe36575493e194d950
Thomas Carpaye <tcarpaye@financeactive.com>
2019-12-03 13:31:45 +0100

    BasePrice extraction

--- src/test/java/fa/ParkingTicket.java
+++ src/test/java/fa/ParkingTicket.java
@@ -1,12 +1,8 @@
 package fa;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 class ParkingTicket {
-    private static final long FULL_HOUR_PRICE = 200L;
-    private static final long HALF_HOUR_PRICE = 150L;
-    private static final Duration FULL_HOUR_PRICE_TIME_LIMIT = Duration.ofHours(4);
     private final LocalDateTime arrival;
     private final LocalDateTime departure;
 
@@ -24,11 +20,4 @@ class ParkingTicket {
         return TimeInParking.of(arrival, departure);
     }
 
-    private class BasePrice {
-        public Long of(TimeInParking timeInParking) {
-            long billableHours = timeInParking.startedHoursUpTo(FULL_HOUR_PRICE_TIME_LIMIT);
-            long billableHalfHours = timeInParking.halfHoursAfter(FULL_HOUR_PRICE_TIME_LIMIT);
-            return billableHours * FULL_HOUR_PRICE + billableHalfHours * HALF_HOUR_PRICE;
-        }
-    }
 }
